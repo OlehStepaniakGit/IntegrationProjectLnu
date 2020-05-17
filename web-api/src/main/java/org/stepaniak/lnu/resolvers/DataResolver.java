6173135018b11c07789bd8ffc546065672dc418d
OlehStepaniakGit <oleh.stepaniak.work@gmail.com>
2020-05-18 00:12:38 +0300

    fixed error with bad interface

--- web-api/src/main/java/org/stepaniak/lnu/resolvers/DataResolver.java
+++ web-api/src/main/java/org/stepaniak/lnu/resolvers/DataResolver.java
@@ -1,6 +1,6 @@
 package org.stepaniak.lnu.resolvers;
 
-import com.coxautodev.graphql.tools.GraphQLResolver;
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import org.springframework.stereotype.Component;
 import org.stepaniak.lnu.entities.Data;
 import org.stepaniak.lnu.service.DataService;
@@ -8,14 +8,14 @@ import org.stepaniak.lnu.service.DataService;
 import java.util.List;
 
 @Component
-public class DataResolver implements GraphQLResolver {
+public class DataResolver implements GraphQLQueryResolver {
     private final DataService dataService;
 
     public DataResolver(DataService dataService) {
         this.dataService = dataService;
     }
 
-    public List<Data> getAllNews() {
+    public List<Data> findAll() {
         return this.dataService.findAll();
     }
 }
